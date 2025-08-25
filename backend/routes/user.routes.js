9c6f33ee216bf84466bff6fac8d75a2dc1a312da
bantikumar111 <bantiprajapti778@gmail.com>
2025-08-25 18:50:52 +0530

    Fix case sensitivity for user.controllers.js import

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { askToAssistant, getCurrentUser, updateAssistant } from "../controllers/user.Controllers.js"
+import { askToAssistant, getCurrentUser, updateAssistant } from "../controllers/user.controllers.js"
 import isAuth from "../middlewares/isAuth.js"
 import upload from "../middlewares/multer.js"
 
